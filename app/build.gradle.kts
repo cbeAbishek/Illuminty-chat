0ba57b652b37e3406d4887800c196c7db84b07e4
cbeAbishek <agsabhishek1907@gmail.com>
2024-11-14 12:10:24 +0530

    setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.illuminty"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.illuminty"
--- app/src/main/java/com/example/illuminty/MainActivity.kt
+++ app/src/main/java/com/example/illuminty/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.illuminty
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var Loginbutton: Button
+    private lateinit var Signupbutton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,13 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        Loginbutton =findViewById<Button>(R.id.Loginbutton)
+        Signupbutton =findViewById<Button>(R.id.Signupbutton)
+
+        Signupbutton.setOnClickListener{
+            val intent = Intent(this, Signup::class.java)
+            startActivity(intent)
         }
     }
 }
